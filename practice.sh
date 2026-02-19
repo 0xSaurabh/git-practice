600037cda14240c9a41a94b44d67998c8f5f8c6a
0xSaurabh <saurabhxgithub@gmail.com>
2026-02-19 04:18:43 +0000

    added a test script

---
+++ practice.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DATE=$(date)
+USER_NAME=$(whoami)
+
+echo "User: $USER_NAME"
+echo "Date: $DATE"
+
+echo ""
+echo "System Information:"
+uname -a
+
+echo ""
+echo "Disk Usage:"
+df -h
+
+echo ""
+echo "Current Directory:"
+pwd
+
+echo ""
+echo "Files in this directory:"
+ls -l

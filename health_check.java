85e8d9805a8295557ea354106ff5a280abf438a0
Cloud User <ec2-user@ip-172-31-38-253.ap-south-1.compute.internal>
2026-02-26 13:58:20 +0000

    added simple java file for test env

---
+++ health_check.java
@@ -0,0 +1,7 @@
+public class HealthCheck {
+
+    public static void main(String[] args) {
+        System.out.println("Application is running in TEST environment.");
+        System.out.println("Health Status: OK");
+    }
+}
